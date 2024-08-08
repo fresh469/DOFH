2f8ef14d0abd67e6c48c5be4111ab5530ce4db91
fresh469 <ms.fresh.cat@gmail.com>
2024-08-08 17:40:26 +0900

    Create main.ts

---
+++ main.ts
@@ -0,0 +1,11 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <title>HTML Example</title>
+  </head>
+  <body>
+    <w3m-button></w3m-button>
+    <script type="module" src="main.ts"></script>
+  </body>
+</html>
